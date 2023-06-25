2ce76402af7e9c3c61ea6144401783d08189db4f
Mono-123 <1321807420@qq.com>
2023-06-25 21:19:27 +0800

    modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,32 @@
 import React, { useState } from 'react'
-import ScoreJson from '../mock/Score.json'
-import Score from './components/score'
+import StudentsJson from '../mock/students.json'
+import Students from './components/Students'
 
 export default () => {
-    const list = ScoreJson.map(score => (
-        <Score key={score.id} id={score.id} studentId={score.studentId} chinese={score.chinese} math={score.math} english={score.english} />
+    let list = StudentsJson.map(students => (
+        <Students key={students.id} id={students.id} name={students.name} gender={students.gender} grade={students.grade} score={students.score} />
     ))
-    let getbyid = ScoreJson.find(score => score.id == 1);
-    const list2 = <Score key={1} id={1} studentId={getbyid.studentId} chinese={getbyid.chinese} math={getbyid.math} english={getbyid.english} />
-    
+
     const [visible, setVisible] = useState(false)
     const [visible2, setVisible2] = useState(false)
     const [visible3, setVisible3] = useState(false)
     const [visible4, setVisible4] = useState(false)
+    const [visible5, setVisible5] = useState(false)
+
     const [input, setInput] = useState('')
     const [input2, setInput2] = useState('')
     const [input3, setInput3] = useState('')
-    const [array, setArray] = useState([])
+
     const [a, setA] = useState()
-
-    function getById(id) {
-        let Array = ScoreJson;
-        for (let i = 0; i < Array.length - 1; i++) {
-            if (Array[i].id == id) {
-                return (Array[i]);
-            }
-        }
-    }
-    const list3 = getById(1);
-
-    // a.addEventListener("click",
-    //     function getById(id) {
-    //         let Array = ScoreJson;
-    //         for (let i = 0; i < Array.length - 1; i++) {
-    //             if (Array[i].id == id) {
-    //                 return (Array[i]);
-    //             }
-    //         }
-    //     }
-    // );
+    const [array, setArray] = useState([])
+    const [array2, setArray2] = useState()
+    
+    const [limit, setLimit] = useState(0)
+    const [offset, setOffset] = useState(5)
 
     return (
         <div>
-            <h1>score</h1>
+            <h1>students</h1>
 
             <div>
                 <p>请输入：<input value={input} onChange={(e) => setInput(e.target.value)}></input></p>
@@ -51,44 +35,61 @@ export default () => {
 
             <div>
                 <p>请输入：<input value={input2} onChange={(e) => setInput2(e.target.value)}></input></p>
-                <p onMouseOver={(e)=>setVisible2(e.target)} onMouseOut={(e)=>setVisible2(false)} >鼠标放在此处将显示你输入的是：{ visible2 &&input2 }</p>
+                <p onMouseOver={(e) => setVisible2(e.target)}>鼠标滑过此处一次将显示你的输入是：{visible2 && input2}</p>
             </div>
 
             <div>
                 <p>请输入：<input value={input3} onChange={(e) => setInput3(e.target.value)}></input></p>
-                <p onMouseOver={(e) => setVisible3(e.target)}>鼠标滑过将显示你输入的是：{visible3 && input2}</p>
+                <p onMouseOver={(e) => setVisible4(e.target)} onMouseOut={(e) => setVisible4(false)} >鼠标放于在此处将显示你的输入是：{visible4 && input3}</p>
             </div>
 
             {/* <input type='checkbox' onChange={(e) => setVisible(e.target.checked)} checked={visible}></input>查询学生成绩 */}
-            <button onClick={(e) => setVisible(e.target)} >查询学生成绩</button>
+            <button onClick={(e) => {
+                setVisible(e.target);
+                setVisible3(false);
+            }} >查询学生成绩</button>
 
             <h4>按id查询</h4>
             请输入需要查找的id: <input type="number" onChange={(e) => setA(e.target.value)} />
             <button onClick={(e) => {
                 setVisible(false);
                 setVisible3(e.target);
-                let getbyid = ScoreJson.find(score => score.id == a);
-                const list2 = <Score key={a} id={a} studentId={getbyid.studentId} chinese={getbyid.chinese} math={getbyid.math} english={getbyid.english} />
-                setArray(list2);
+                let Array = StudentsJson;
+                for (let i = 0; i < Array.length - 1; i++) {
+                    if (Array[i].id == a) {
+                        setArray(list[i]);
+                        break;
+                    } else {
+                        setArray("not found")
+                    }
+                }
             }} >查询</button>
 
+            <h4>分页查询</h4>
+            limit：<input type="number" onChange={(e) => setLimit(e.target.value)} />
+            offset：<input type="number" onChange={(e) => setOffset(e.target.value)} />
+            <button onClick={(e) => {
+                setVisible(false);
+                setVisible5(e.target);
+                for (let i = limit; i < offset - 1; i++) {
+                        setArray2(list[i]);     
+                }
+            }} >获取</button>
 
             <table>
                 <thead>
                     <tr>
                         <th>Id</th>
-                        <th>学生学号</th>
-                        <th>语文成绩</th>
-                        <th>数学成绩</th>
-                        <th>英语成绩</th>
+                        <th>名称</th>
+                        <th>性别</th>
+                        <th>年级</th>
+                        <th>分数</th>
                     </tr>
                 </thead>
                 <tbody id="table">
                     {visible && list}
-                    {array}
-                    {list2}
-                    {[list3]}
-                    {a}
+                    {visible3 && array}
+                    {visible5 && array2}
                 </tbody>
             </table>
 
