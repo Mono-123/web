02ceef69ce259baa65c63b92d27cdf8ed7135e49
Mono-123 <1321807420@qq.com>
2023-06-28 23:29:23 +0800

    modify stuentsId

--- src/components/StudentsId.jsx
+++ src/components/StudentsId.jsx
@@ -1,9 +1,10 @@
 export default (record) => {
+    const grades = ['一年级', '二年级', '三年级', '四年级']
     return (
         <p>
             姓名：{record.name}<br />
-            性别：{record.gender}<br />
-            年级：{record.garde}<br />
+            性别：{(record.gender ? '男' : '女  ')}<br />
+            年级：{grades[record.grade - 1]}<br />
             分数：{record.score}
         </p>
     )
