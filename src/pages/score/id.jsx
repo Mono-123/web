96441a595e5c60b50eaa43133fe67e32b4dfd40d
Mono-123 <1321807420@qq.com>
2023-06-28 16:05:46 +0800

    modify score

--- src/pages/score/id.jsx
+++ src/pages/score/id.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom'
 import ScoreJson from '../../../mock/Score.json'
 import Score from '../../components/score'
